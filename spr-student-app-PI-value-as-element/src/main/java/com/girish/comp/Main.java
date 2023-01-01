994d0b7599216f7b79c396fda9c525f4e2b99f83
Girish Giradkar <girishgiradkar824@gmail.com>
2023-01-01 23:58:37 +0530

    first

---
+++ spr-student-app-PI-value-as-element/src/main/java/com/girish/comp/Main.java
@@ -0,0 +1,15 @@
+package com.girish.comp;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("cfg.xml");
+		Student s1 = (Student) context.getBean("a");
+		System.out.println(s1);
+		Student s2 = (Student) context.getBean("student2");
+		System.out.println(s2);
+	}
+}
